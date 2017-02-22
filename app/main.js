6f79d7f2d27c8b6f140b2ddf2bc1a96813c1a541
chipanda <chipanda7@outlook.com>
2017-02-22 10:57:36 +0800

    import问题修复
    删除废弃引用

--- app/main.js
+++ app/main.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import {Router, Route, IndexRedirect, hashHistory, browserHistory, applyRouterMiddleware} from 'react-router'
-import { useScroll } from 'react-router-scroll'
+import {Router, Route, IndexRedirect, hashHistory, browserHistory} from 'react-router'
 import App from './containers/App'
 import GoodsContainer from './containers/GoodsContainer'
 import RatingsContainer from './containers/RatingsContainer'
@@ -27,7 +26,7 @@ const manageScroll = (prevRouterProps, { routes }) => {
 }
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={hashHistory} render={applyRouterMiddleware(useScroll(manageScroll))}>
+        <Router history={hashHistory}>
             <Route path="/" component={App}>
                 <IndexRedirect to="/goods"/>
                 <Route path="goods" component={GoodsContainer}></Route>
